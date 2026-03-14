6c12111a84ee57b0def8e89350865620a16cf52d
smit23patel <smit23patel@gmail.com>
2026-03-14 10:02:38 +0530

    commit

--- CODE/practial1.py
+++
@@ -1,7 +0,0 @@
-income = int(input("Enter income: "))
-expenses = int(input("Enter expenses: "))
-
-bal = income - expenses
-print("balance", bal)
-print("Income->", income)
-print("Expenses->", expenses)
